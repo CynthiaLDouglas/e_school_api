1078159aacf6a2bc2c0cc314c4a842020ff56a69
Cyn-D <cynthia.l.douglas@gmail.com>
2020-09-21 09:47:46 -0400

    deploying a second time

--- api/models/registration.py
+++ api/models/registration.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Registration(models.Model):
     semester = models.CharField(max_length=10)
     course_name = models.ForeignKey('Course', on_delete=models.CASCADE)
-    student_enrolled = models.ForeignKey('User', on_delete=models.CASCADE)
+    student_enrolled = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.student_enrolled} enrolled into {self.course_name}"
